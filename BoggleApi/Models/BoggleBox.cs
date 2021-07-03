715f6d55dcadae0461f5c37a16a853b45a60f330
Micky <mickycheng16@gmail.com>
2021-07-03 20:14:47 +0200

    Initial commit 2

---
+++ BoggleApi/Models/BoggleBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoggleApi.Models
+{
+    public class BoggleBox
+    {
+        public Guid BoggleBoxID { get; set; }
+        public List<List<Die>> Dies { get; set; }
+
+    }
+
+    public class Die
+    {
+        public Die(char Value)
+        {
+            this.Value = Value;
+        }
+        public char Value { get; set; }
+    }
+}
